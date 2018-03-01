51177c72916585ee525e203b8c60c053f63de128
Dariusz <dariusz.rochowiak@gmail.com>
2018-03-01 17:31:03 +0100

    Nested component + service

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 
 @Component({
@@ -7,41 +9,43 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-  ngOnInit(): void {
-  }
-    pageTitle: string = 'Product List';
-    products: any[] = [
-      {
-        "productId": 1,
-        "productName": "Leaf Rake",
-        "productCode": "GDN-0011",
-        "releaseDate": "March 19, 2016",
-        "description": "Leaf rake with 48-inch wooden handle.",
-        "price": 19.95,
-        "starRating": 3.2,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-    },
-    {
-        "productId": 2,
-        "productName": "Garden Cart",
-        "productCode": "GDN-0023",
-        "releaseDate": "March 18, 2016",
-        "description": "15 gallon capacity rolling garden cart",
-        "price": 32.99,
-        "starRating": 4.2,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+
+    constructor(private _productService: ProductService) {
     }
-    ];
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) =>
+            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
+
+    ngOnInit(): void {
+        this.products = this._productService.getProducts();
+        this.filteredProducts = this.products;
+    }
+
+    pageTitle: string = 'Product List';
+    products: IProduct[] = [];
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
     errorMessage: string;
-
     _listFilter: string;
+    filteredProducts: IProduct[];
+
     get listFilter(): string {
         return this._listFilter;
     }
     set listFilter(value: string) {
         this._listFilter = value;
-    }    
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
+
+    onRatingClicked(message: string): void {
+        this.pageTitle = message;
+    }
 }
